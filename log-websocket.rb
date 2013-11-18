516f429cf494a841a0c730f62f6b52d4229a854e
Jaime Giraldo <sposmen@gmail.com>
2013-11-18 15:17:05 -0500

    Includding Cassandra

--- log-websocket.rb
+++ log-websocket.rb
@@ -2,6 +2,7 @@ require 'em-websocket'
 require 'mongo'
 require 'pg'
 require 'mysql'
+require 'cql'
 
 include Mongo
 
@@ -13,11 +14,16 @@ include Mongo
 
 @my = Mysql.new('localhost', 'root', 'pruebas', 'pruebas')
 
+@cassandra = Cql::Client.connect(hosts: ['localhost'])
+@cassandra.use('pruebas')
+@uuid = Cql::TimeUuid::Generator.new
 timer_logMongo = File.open('timerMongo.log', 'w')
 timer_logPG = File.open('timerPG.log', 'w')
 timer_logMy = File.open('timerMySQL.log', 'w')
+timer_logCassandra = File.open('timerCassandra.log', 'w')
 
 timer_logFile = File.open('timerFile.log', 'w')
+timer_logFileMem = File.open('timerFileMem.log', 'w')
 
 EM.run {
   EM::WebSocket.run(:host => "0.0.0.0", :port => 8080) do |ws|
@@ -51,6 +57,14 @@ EM.run {
       timer_logMy.seek(0,IO::SEEK_END)
       timer_logMy.write("#{Time.now - beginning}\n")
       
+      #Cassandra
+      beginning = Time.now
+      st = @cassandra.prepare("INSERT INTO xy_coordenates(id, user_id, msg) VALUES (?,?,?)")
+      st.execute(@uuid.next, 1, msg)
+      timer_logCassandra.seek(0,IO::SEEK_END)
+      timer_logCassandra.write("#{Time.now - beginning}\n")
+
+
       #File
       beginning = Time.now
       File.open('general.log', 'a') { |f|
@@ -58,6 +72,14 @@ EM.run {
       }
       timer_logFile.seek(0,IO::SEEK_END)
       timer_logFile.write("#{Time.now - beginning}\n")
+      
+      #File in MEM
+      beginning = Time.now
+      File.open('logmem/general.log', 'a') { |f|
+        f.write('1;' + '"'+msg+'"'+"\n")
+      }
+      timer_logFileMem.seek(0,IO::SEEK_END)
+      timer_logFileMem.write("#{Time.now - beginning}\n")
       
     }
   end
